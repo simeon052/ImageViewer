6ec1157235df8be1160b8814ad0faf2f6e7c53be
SIMEON <simeon_the_city_who_fought@outlook.com>
2015-11-29 18:06:42 +0900

    Initial Commit
    Can open and view Image file

---
+++ ImageViewer/MainPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Windows.Storage;
+using Windows.Storage.Pickers;
+using Windows.Storage.Streams;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media.Imaging;
+
+// 空白ページのアイテム テンプレートについては、http://go.microsoft.com/fwlink/?LinkId=234238 を参照してください
+
+namespace ImageViewer
+{
+    /// <summary>
+    /// それ自体で使用できる空白ページまたはフレーム内に移動できる空白ページ。
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+
+        private async void LoadButton_Click(object sender, RoutedEventArgs e)
+        {
+            var picker = new FileOpenPicker();
+            picker.FileTypeFilter.Add(".png");
+            StorageFile file = await picker.PickSingleFileAsync();
+            if (file == null) return;
+
+            IRandomAccessStream stream = await file.OpenReadAsync();
+
+            var bitmap = new BitmapImage();
+            bitmap.SetSource(stream);
+            this.image.Source = bitmap;
+        }
+    }
+}
